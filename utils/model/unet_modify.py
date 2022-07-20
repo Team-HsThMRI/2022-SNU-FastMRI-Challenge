ef0a8ce8514a975454fd5e253f6164887335eb70
Lee Hyunsoo <frogyunmax@gmail.com>
2022-07-20 22:15:12 +0900

    add model loading code

--- utils/model/unet_modify.py
+++ utils/model/unet_modify.py
@@ -18,14 +18,16 @@ class Unet(nn.Module):
         # self.conv5 = ConvBlock_1step(64, 32)
         # self.last_block = nn.Conv2d(32, out_chans, kernel_size=1)
 
-        self.down1 = Down(64, 128)
-        self.down2 = Down(128, 256)
-        self.down3 = Down(256, 512)
-        self.down4 = Down(512, 1024)
-        self.up1 = Up(1024, 512)
-        self.up2 = Up(512, 256)
-        self.up3 = Up(256, 128)
-        self.up4 = Up(128, 64)
+        self.down1 = Down(64, 128, 0)
+        self.down2 = Down(128, 256, 0)
+        self.down3 = Down(256, 512, 0)
+        self.down4 = Down(512, 1024, 0)
+        self.down5 = Down(1024, 2048, 0)
+        self.up1 = Up(2048, 1024, 0)
+        self.up2 = Up(1024, 512, 0)
+        self.up3 = Up(512, 256, 0)
+        self.up4 = Up(256, 128, 0)
+        self.up5 = Up(128, 64, 0)
         self.last_block = nn.Conv2d(64, out_chans, kernel_size=1)
 
     def norm(self, x):
@@ -54,12 +56,14 @@ class Unet(nn.Module):
         d2 = self.down1(d1)
         d3 = self.down2(d2)
         d4 = self.down3(d3)
-        m0 = self.down4(d4)
-        u1 = self.up1(m0, d4)
-        u2 = self.up2(u1, d3)
-        u3 = self.up3(u2, d2)
-        u4 = self.up4(u3, d1)
-        output = self.last_block(u4)
+        d5 = self.down4(d4)
+        m0 = self.down5(d5)
+        u1 = self.up1(m0, d5)
+        u2 = self.up2(u1, d4)
+        u3 = self.up3(u2, d3)
+        u4 = self.up4(u3, d2)
+        u5 = self.up5(u4, d1)
+        output = self.last_block(u5)
 
         # output = self.last_block(c5)
         output = output.squeeze(1)
@@ -105,12 +109,13 @@ class ConvBlock(nn.Module):
 
 class Down(nn.Module):
 
-    def __init__(self, in_chans, out_chans):
+    def __init__(self, in_chans, out_chans, p):
         super().__init__()
         self.in_chans = in_chans
         self.out_chans = out_chans
         self.layers = nn.Sequential(
             nn.MaxPool2d(2),
+            nn.Dropout(p),
             ConvBlock(in_chans, out_chans)
         )
 
@@ -120,14 +125,16 @@ class Down(nn.Module):
 
 class Up(nn.Module):
 
-    def __init__(self, in_chans, out_chans):
+    def __init__(self, in_chans, out_chans, p):
         super().__init__()
         self.in_chans = in_chans
         self.out_chans = out_chans
         self.up = nn.ConvTranspose2d(in_chans, in_chans // 2, kernel_size=2, stride=2)
         self.conv = ConvBlock(in_chans, out_chans)
+        self.dropout = nn.Dropout(p)
 
     def forward(self, x, concat_input):
         x = self.up(x)
         concat_output = torch.cat([concat_input, x], dim=1)
+        concat_output = self.dropout(concat_output)
         return self.conv(concat_output)
