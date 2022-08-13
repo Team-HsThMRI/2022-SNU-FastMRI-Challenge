3375bf164d35f1981f375104b318814e8a5a5fcc
Lee Hyunsoo <frogyunmax@gmail.com>
2022-08-14 06:23:04 +0900

    Use lr_scheduler for training Varnet

--- FastMRI_challenge-2022_varnet/train.py
+++ FastMRI_challenge-2022_varnet/train.py
@@ -5,23 +5,23 @@ import os, sys
 if os.getcwd() + '/utils/model/' not in sys.path:
     sys.path.insert(1, os.getcwd() + '/utils/model/')
 
-from utils.learning.train_part import train
+from utils.learning.train_part_lrdecay import train
 from pathlib import Path
-
+from utils.model.random_seed import seed_fix
 
 def parse():
     parser = argparse.ArgumentParser(description='Train Unet on FastMRI challenge Images',
                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-g', '--GPU-NUM', type=int, default=0, help='GPU number to allocate')
     parser.add_argument('-b', '--batch-size', type=int, default=1, help='Batch size')
-    parser.add_argument('-e', '--num-epochs', type=int, default=1, help='Number of epochs')
-    parser.add_argument('-l', '--lr', type=float, default=1e-3, help='Learning rate')
+    parser.add_argument('-e', '--num-epochs', type=int, default=20, help='Number of epochs')
+    parser.add_argument('-l', '--lr', type=float, default=0.0005, help='Learning rate')
     parser.add_argument('-r', '--report-interval', type=int, default=500, help='Report interval')
     parser.add_argument('-n', '--net-name', type=Path, default='test_varnet', help='Name of network')
     parser.add_argument('-t', '--data-path-train', type=Path, default='/root/input/train', help='Directory of train data')
     parser.add_argument('-v', '--data-path-val', type=Path, default='/root/input/val/', help='Directory of validation data')
     
-    parser.add_argument('--cascade', type=int, default=1, help='Number of cascades | Should be less than 12') ## important hyperparameter
+    parser.add_argument('--cascade', type=int, default=2, help='Number of cascades | Should be less than 12') ## important hyperparameter
     parser.add_argument('--input-key', type=str, default='kspace', help='Name of input key')
     parser.add_argument('--target-key', type=str, default='image_label', help='Name of target key')
     parser.add_argument('--max-key', type=str, default='max', help='Name of max key in attributes')
@@ -38,4 +38,5 @@ if __name__ == '__main__':
     args.exp_dir.mkdir(parents=True, exist_ok=True)
     args.val_dir.mkdir(parents=True, exist_ok=True)
 
+    seed_fix(42)
     train(args)
